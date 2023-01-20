69a7ebf136b031c3772eb2ad62c4d1f5f3ad1af3
97Fekim <rlatmdcjf9@gmail.com>
2023-01-20 11:10:33 +0900

    json2

---
+++ src/main/java/com/psc/sample/q103/dto/CoinMarket.java
@@ -0,0 +1,16 @@
+package com.psc.sample.q103.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class CoinMarket {
+
+    String market;
+    String korean_name;
+    String english_name;
+
+}
